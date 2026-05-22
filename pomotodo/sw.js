4d579a4639aaf89f942258f9cf8296cee521f571
185www <185www@users.noreply.github.com>
2026-05-22 13:07:38 +0000

    deploy: 714b0f2c1293a750f27bad8b20f769d42b2d2f7a

--- pomotodo/sw.js
+++ pomotodo/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pomotodo-v19-parentBadge-fix';
+const CACHE = 'pomotodo-v20-v5-p0-features';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './timer-worker.js', './manifest.json'];
 
 self.addEventListener('install', e => {
