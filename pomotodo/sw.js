45a969415bbc3ce6f8e640604c8fcda6ae55895f
185www <185www@users.noreply.github.com>
2026-05-22 14:09:34 +0000

    deploy: cf254267e3357f3d59f62b0dd89bf270f1f9c06d

--- pomotodo/sw.js
+++ pomotodo/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pomotodo-v20-v5-p0-features';
+const CACHE = 'pomotodo-v20-phase1';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './timer-worker.js', './manifest.json'];
 
 self.addEventListener('install', e => {
