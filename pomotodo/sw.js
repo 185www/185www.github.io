5d6639ecb6e93b0d709544099e0023bbfb81dd6f
Super Z <superz@z.ai>
2026-05-31 05:34:48 +0000

    chore: bump SW cache version to v9-fix to force cache refresh

--- pomotodo/sw.js
+++ pomotodo/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pomotodo-v8-gaokao';
+const CACHE = 'pomotodo-v9-fix';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./timer-worker.js','./favicon.svg','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',e=>{
